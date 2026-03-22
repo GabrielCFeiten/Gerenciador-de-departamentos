e30430d60c8d6dea6c955ae076f02cc8ab8d21d2
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-22 15:06:36 -0300

    Repository add

---
+++ src/main/java/org/cf/gerenciadordedepartamentos/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package org.cf.gerenciadordedepartamentos.repositories;
+
+import org.cf.gerenciadordedepartamentos.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}

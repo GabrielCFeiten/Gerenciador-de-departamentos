63860ee68dfd23d19b7becaeb181ff96c46267b7
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-22 15:04:34 -0300

    Model add

---
+++ src/main/java/org/cf/gerenciadordedepartamentos/GerenciadorDeDepartamentosApplication.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordedepartamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeDepartamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeDepartamentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cf/gerenciadordedepartamentos/GerenciadorDeDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordedepartamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeDepartamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
